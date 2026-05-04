35cbd3175c711674c6fc82705976e317e0b8a243
nardiniryan5-ai <nardiniryan5@gmail.com>
2026-05-04 14:11:42 +0000

    v2

--- seletor.py
+++ seletor.py
@@ -1,8 +1,59 @@
 #--Seletor de Caracteres--
-letra = input("digite a Letra")
+print("Bem Vindo ao Seletor de Caracteres: ")
+print("aqui voce pode escolher uma letra do alfabeto e o programa ira moestrar a letra escrita ")
+
+letra = input("digite a Letra: ").lower().upper()
 if letra == "A":
-  print("Voce escolheu a letra A")
+  print("Voce escolheu a letra A😈🍆: ")
 elif letra =="B":
-  print("Voce escolheu a letra B")
+  print("Voce escolheu a letra B😈🍆: ")
+elif letra =="C":
+  print("Voce escolheu a letra C😈🍆: ")
+elif letra =="D":
+  print("Voce escolheu a letra D😈🍆: ")
+elif letra =="E":
+  print("Voce escolheu a letra E😈🍆: ")
+elif letra =="F":
+  print("Voce escolheu a letra F😈🍆: ")
+elif letra =="G":
+  print("Voce escolheu a letra G😈🍆: ")
+elif letra =="H":
+  print("Voce escolheu a letra H😈🍆: ")
+elif letra =="I":
+  print("Voce escolheu a letra I😈🍆: ")
+elif letra =="J":
+  print("Voce escolheu a letra J😈🍆: ")
+elif letra =="K":
+  print("Voce escolheu a letra K😈🍆: ")
+elif letra =="L":
+  print("Voce escolheu a letra L😈🍆: ")
+elif letra =="M":
+  print("Voce escolheu a letra M😈🍆: ")
+elif letra =="N":
+  print("Voce escolheu a letra N:😈🍆 ")
+elif letra =="O":
+  print("Voce escolheu a letra O:😈🍆 ")
+elif letra =="P":
+  print("Voce escolheu a letra P:😈🍆 ")
+elif letra =="Q":
+  print("Voce escolheu a letra Q:😈🍆 ")
+elif letra =="R":
+  print("Voce escolheu a letra R:😈🍆 ")
+elif letra =="S":
+  print("Voce escolheu a letra S:😈🍆 ")
+elif letra =="T":
+  print("Voce escolheu a letra T:😈🍆 ")
+elif letra =="U":
+  print("Voce escolheu a letra U:😈🍆 ")
+elif letra =="V":
+  print("Voce escolheu a letra V:😈🍆 ")
+elif letra =="W":
+  print("Voce escolheu a letra W:😈🍆 ")
+elif letra =="X":
+  print("Voce escolheu a letra X:😈🍆")
+elif letra =="Y":
+  print("Voce escolheu a letra Y:😈🍆 ")
+elif letra =="Z":
+  print("Voce escolheu a letra Z:😈🍆 ")
 else:
-  print(""Essa opçao não está disponivel")
+  print("Essa opçao não está disponivel")
--- var_sentinela.py
+++ var_sentinela.py
@@ -1,1 +1,6 @@
-
+#Exemplo de Uso da Variavel Sentinela
+while True:
+    comando = input("Digite um comando: ")
+    if comando == "sair":
+        break
+    print(f"Executado {comando}")
