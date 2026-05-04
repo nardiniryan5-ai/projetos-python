32023a5213a32327935896008fdd2256a9f710b4
nardiniryan5-ai <nardiniryan5@gmail.com>
2026-05-04 09:33:39 -0300

    v1
    crie o arquivo seletor.py e criei a parte base do programa

---
+++ seletor.py
@@ -0,0 +1,8 @@
+#--Seletor de Caracteres--
+letra = input("digite a Letra")
+if letra == "A":
+  print("Voce escolheu a letra A")
+elif letra =="B":
+  print("Voce escolheu a letra B")
+else:
+  print(""Essa opçao não está disponivel")
