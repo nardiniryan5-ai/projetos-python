8411a6538fa314a0019deb15c22240b83f511f76
nardiniryan5-ai <nardiniryan5@gmail.com>
2026-05-04 15:59:05 +0000

    v1

---
+++ quizz.py
@@ -0,0 +1,17 @@
+print("Bem Vindo ao Quiz de Astronomia!")
+
+questao1 = input("2. Quantos satélites naturais (luas) Marte possui? \nA) Nenhum \nB) Dois: Fobos e Deimos \nC) Um: Tritão \nD) Vinte ").upper()
+if questao1 == "B":
+    print("Resposta correta!👍")
+else:
+    print("Resposta incorreta! A resposta correta é B) Dois: Fobos e Deimos.")
+questao2 = input("1. Qual é o maior planeta do nosso Sistema Solar? \nA) Saturno \nB) Júpiter \nC) Terra: ").upper()
+if questao2 == "B":
+    print("Resposta correta!👍")
+else:
+    print("Resposta incorreta! A resposta correta é B) Júpiter.")
+questao3 = input("3. Qual planeta leva mais tempo para completar uma volta ao redor do Sol (translação)? \nA) Terra \nB) Mercúrio \nC) Netuno \nD) Júpiter ").upper()
+if questao3 == "C":
+    print("Resposta correta!")
+else:
+    print("Resposta incorreta! A resposta correta é C) Netuno.")
