478a96840a0b9aa3a5123e84c0b4f4271b4949f6
nardiniryan5-ai <nardiniryan5@gmail.com>
2026-05-25 12:13:40 +0000

    v1

---
+++ cabeçalho.py
@@ -0,0 +1,7 @@
+def exibir_cabecalho():
+    print("=" * 50)
+    print("SISTEMA DE GESTÃO ESCOLAR")
+    print("versão 1.0 - Desenvolvido no CodeSpace")
+    print("=" * 50)
+exibir_cabecalho()
+print("Menu Principal: ")
