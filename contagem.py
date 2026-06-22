056c987199ffb02692801fb29be2b9f3f51a531c
nardiniryan5-ai <nardiniryan5@gmail.com>
2026-06-22 12:08:02 +0000

    v1

--- contagem.py
+++ contagem.py
@@ -2,13 +2,13 @@
 def maior_palavra(texto):
     for p in ", ! ? . ; : ":
         texto = texto.replace(p, "")
-palavra = texto.split()
-if not palavra:
-  return " "  
-tamanho = ""
-for frase in palavra:
-    if len(frase) >= len(tamanho):
-        tamanho = frase
-        
-return tamanho
+    palavra = texto.split()
+    if not palavra:
+        return " "
+    tamanho = ""
+    for frase in palavra:
+        if len(frase) >= len(tamanho):
+            tamanho = frase
+    return tamanho
+
 print(maior_palavra("texto reflexão"))
---
+++ filmes.py
@@ -0,0 +1,3 @@
+filmes = ["matrix", "star wars", "avatar    "]
+print(filmes[0])
+print(filmes[2])
